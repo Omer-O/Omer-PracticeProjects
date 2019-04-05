038c81835258298d9d970f6e93d78f30aaf624cd
ohanaomer1@gmail.com <ohanaomer1@gmail.com>
2019-04-05 13:46:42 +0200

    Submit assesment

---
+++ stickMan.js
@@ -0,0 +1,16 @@
+var x;
+var xx;
+
+x = 5060;
+
+function timesTwo(num) {
+    return num * 2;
+}
+
+console.log
+
+for ( var i = 0; i < numbers.length; i++ ) {
+    console.log(numbers[i]);
+}
+
+xx = timeTwo(x)
