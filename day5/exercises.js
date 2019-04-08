b05a804c06fa161aae8332c8d744d0ba3654a570
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-08 09:10:28 +0200

    remove redundant if in invertCase fn

--- day5/exercises.js
+++ day5/exercises.js
@@ -46,7 +46,7 @@ function invertCase(str) {
     for (var i = 0; i < str.length; i++) {
         if (str[i] === str[i].toLowerCase()) {
             newStr += str[i].toUpperCase();
-        } else if (str[i] === str[i].toUpperCase()) {
+        } else {
             newStr += str[i].toLowerCase();
         }
     }
