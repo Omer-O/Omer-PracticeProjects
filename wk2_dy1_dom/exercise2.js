1f6d136d56909329415e220e45fc999e0d488591
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-08 14:38:16 +0200

    exercise 2 first attempt

---
+++ wk2_dy1_dom/exercise2.js
@@ -0,0 +1,13 @@
+/*
+2)
+Write a function that expects a string representing a class name to be passed as a parameter.
+The function should return an array containing all the elements in the document that have the class that was passed in.
+*/
+(function() {
+    function returnArray(classStr) {
+        var myArray = document.getElementsByClassName(classStr);
+        console.log(myArray);
+    }
+
+    returnArray("text");
+})();
