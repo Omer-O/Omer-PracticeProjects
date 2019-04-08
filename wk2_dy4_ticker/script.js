8fa6d840de2e1d2143179752641c7ff02e21f8ca
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-08 16:42:26 +0200

    working draft

--- wk2_dy4_ticker/script.js
+++ wk2_dy4_ticker/script.js
@@ -3,12 +3,22 @@
     var links = headlines.getElementsByTagName("A"); //number of links
     var left = headlines.offsetLeft;
 
+    console.log("Number of links: ", links);
+    console.log("First link: ", links[0]);
+    console.log("Lenght of 1st link: ", links[0].text.lenght);
+    console.log("offsetWidth: ", links[0].offsetWidth);
     function moveHeadlineLines() {
-        console.log(left--);
+        left--;
+        if (left <= -links[0].offsetWidth) {
+            console.log("First link gone!");
+            left += links[0].offsetWidth;
+            var removed = headlines.removeChild(links[0]);
+            headlines.appendChild(removed);
+        }
+        //move headlines to left
         headlines.style.left = left + "px";
         // }
         requestAnimationFrame(moveHeadlineLines); //sync with page refresh
     }
-
     moveHeadlineLines();
 })();
