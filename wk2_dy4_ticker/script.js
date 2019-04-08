22fad39d0b57c5e90b2993a2547af71cff635f59
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-08 16:49:06 +0200

    tidy up comments and code

--- wk2_dy4_ticker/script.js
+++ wk2_dy4_ticker/script.js
@@ -3,21 +3,13 @@
     var links = headlines.getElementsByTagName("A"); //number of links
     var left = headlines.offsetLeft;
 
-    console.log("Number of links: ", links);
-    console.log("First link: ", links[0]);
-    console.log("Lenght of 1st link: ", links[0].text.lenght);
-    console.log("offsetWidth: ", links[0].offsetWidth);
     function moveHeadlineLines() {
         left--;
         if (left <= -links[0].offsetWidth) {
-            console.log("First link gone!");
             left += links[0].offsetWidth;
-            var removed = headlines.removeChild(links[0]);
-            headlines.appendChild(removed);
+            headlines.appendChild(headlines.removeChild(links[0]));
         }
-        //move headlines to left
-        headlines.style.left = left + "px";
-        // }
+        headlines.style.left = left + "px"; //move headlines to left
         requestAnimationFrame(moveHeadlineLines); //sync with page refresh
     }
     moveHeadlineLines();
