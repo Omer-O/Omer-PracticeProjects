6f8ead5626f20518ad505261f104d92e336691e9
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-09 16:00:14 +0200

    ticker part 2 exercise

--- wk2_dy4_ticker/script.js
+++ wk2_dy4_ticker/script.js
@@ -2,6 +2,7 @@
     var headlines = document.getElementById("headlines");
     var links = headlines.getElementsByTagName("A"); //number of links
     var left = headlines.offsetLeft;
+    var animationId;
 
     function moveHeadlineLines() {
         left--;
@@ -10,7 +11,19 @@
             headlines.appendChild(headlines.removeChild(links[0]));
         }
         headlines.style.left = left + "px"; //move headlines to left
-        requestAnimationFrame(moveHeadlineLines); //sync with page refresh
+        animationId = requestAnimationFrame(moveHeadlineLines); //sync with page refresh
+        // console.log(animationId);
     }
+
+    headlines.addEventListener("mouseenter", function() {
+        cancelAnimationFrame(animationId);
+
+        // console.log(animationId);
+    });
+
+    headlines.addEventListener("mouseleave", function() {
+        moveHeadlineLines();
+    });
+
     moveHeadlineLines();
 })();
