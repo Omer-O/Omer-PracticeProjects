267410670f0d393bcefbfc51c178e271d18cb61a
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-09 17:28:10 +0200

    improve function with offset

--- wk2_dy2_dom_events/exercise1/script.js
+++ wk2_dy2_dom_events/exercise1/script.js
@@ -3,13 +3,12 @@ Make a page that has on it an element that is 100px by 100px in size, has absolu
 and has a solid background color. Add an event handler that makes this box center itself directly under
 the user's mouse pointer as it is moved across the screen
 */
-console.log("test test");
 // var element = document.getElementById("element");
 
 document.addEventListener("mousemove", function(e) {
     var x = e.clientX;
     var y = e.clientY;
     var element = document.getElementById("element");
-    element.style.left = x - 50 + "px";
-    element.style.top = y - 50 + "px";
+    element.style.left = x - element.offsetWidth / 2 + "px";
+    element.style.top = y - element.offsetHeight / 2 + "px";
 });
