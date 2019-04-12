73bd411d221c107ec638b7f4c17886860ad2a4ca
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-12 14:47:03 +0200

    clicked dot update current img counter

--- week02/wk2_carousel/script.js
+++ week02/wk2_carousel/script.js
@@ -3,13 +3,17 @@
     var dots = document.getElementsByClassName("dot");
     // console.log("number of kitties: ", kitties.length);
     var cur = 0;
+    var timer;
+    var transitioning;
+
     setTimeout(moveKitties, 3000); //kick off only
 
     document.addEventListener("transitionend", function fn(e) {
         if (e.target.classList.contains("exit")) {
             e.target.classList.remove("exit");
             // console.log("transitionend");
-            setTimeout(moveKitties, 2000);
+            timer = setTimeout(moveKitties, 2000);
+            transitioning = false;
         }
     });
     // -------------- clikable dot ver 1 -----------------
@@ -27,6 +31,16 @@
     [].slice.call(dots).forEach(function(dot, i) {
         dots[i].addEventListener("click", function(e) {
             console.log(i);
+            //if dots correspond to currnet kitty
+            if (e.target.classList.contains("on")) {
+                return;
+            }
+            if (transitioning) {
+                return;
+            }
+            // cancel scheduled incoming kitty
+            clearTimeout(timer);
+            moveKitties(i);
         });
     });
     // -------------- clikable dot ver 3 -----------------
@@ -38,15 +52,20 @@
     });
     */
 
-    function moveKitties() {
+    function moveKitties(next) {
+        transitioning = true;
         dots[cur].classList.remove("on");
         // console.log("the current one is: " + cur);
         kitties[cur].classList.remove("onscreen");
         kitties[cur].classList.add("exit");
-        cur++;
+        //
+        if (next >= 0) {
+            cur = next;
+        } else cur++;
         if (cur >= kitties.length) {
             cur = 0;
         }
+
         // console.log("the next one is: " + cur);
         dots[cur].classList.add("on");
         kitties[cur].classList.add("onscreen");
