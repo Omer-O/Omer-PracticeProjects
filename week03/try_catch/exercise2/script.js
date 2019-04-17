27681dbd10f9e9f53124d8e1b0ac5949caecda02
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-17 13:20:32 +0200

    translate function exercise

---
+++ week03/try_catch/exercise2/script.js
@@ -0,0 +1,32 @@
+function translateNumberToGerman() {
+    var number;
+    var transloation = [
+        "eins",
+        "zwei",
+        "drei",
+        "vier",
+        "fünf",
+        "sechs",
+        "sieben",
+        "acht",
+        "neun",
+        "zehn"
+    ];
+    try {
+        number = askForNumber();
+        alert(transloation[number - 1]);
+    } catch (error) {
+        console.log(error);
+        translateNumberToGerman();
+    }
+}
+
+function askForNumber() {
+    var num = prompt("Please enter a number between 1 and 10");
+    if (num >= 1 && num <= 10 && num == parseInt(num)) {
+        return num;
+    }
+    throw new Error("Bad number");
+}
+
+translateNumberToGerman();
