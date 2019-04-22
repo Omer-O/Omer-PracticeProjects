7aad0d236837fe97c040fab254787cb038bcefad
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-22 13:01:15 +0200

    converting an diagonal array into slots issue

--- week03/connect_four/script.js
+++ week03/connect_four/script.js
@@ -1,85 +1,100 @@
-(function() {
-    var currentPlayer = "player1";
+// (function() {
+var slot = $(".slot");
+const diagonalsIndex = [
+    [0, 7, 14, 21, 28, 35],
+    [1, 8, 15, 22, 29],
+    [2, 9, 16, 23],
+    [6, 13, 20, 27, 34, 41],
+    [12, 19, 26, 33, 40],
+    [18, 25, 32, 39],
+    [18, 13, 8, 3],
+    [24, 19, 14, 9, 4],
+    [30, 25, 20, 15, 10, 5],
+    [36, 31, 26, 21, 16, 11],
+    [37, 32, 27, 22, 17],
+    [38, 33, 28, 23]
+];
 
-    $(".column").on("click", function(e) {
-        var slotsInColumn = $(e.currentTarget).find(".slot");
-        for (var i = 5; i >= 0; i--) {
-            if (
-                !slotsInColumn.eq(i).hasClass("player1") &&
-                !slotsInColumn.eq(i).hasClass("player2")
-            ) {
-                slotsInColumn.eq(i).addClass(currentPlayer);
-                break;
-            }
-        }
-        // gone thru hole loop without breake = column is full
-        // i is the row5
-        if (i == -1) {
-            return;
-        }
-        var slotsInRow = $(".column").find(".row" + i);
-        console.log("slots in row: ", slotsInRow);
-        console.log("slots in column: ", slotsInColumn);
+var currentPlayer = "player1";
 
-        var slotsInDiagonals = [];
-        for (var j = 0; j < diagonalsIndex.length; j++) {
-            for (var k = 0; k < diagonalsIndex[j].length; k++) {
-                slotsInDiagonals.push($(".slot").eq(diagonalsIndex[j][k]));
-            }
-        }
+// ----------- ISSUE here, how to convert the array above with diagonals indexes
+//             into seriers of slots which could be passed to checkForVictory function?
+var slotsInDiagonal = new Array(diagonalsIndex.length);
+for (var w = 0; w < diagonalsIndex.length; w++) {
+    slotsInDiagonal[w] = [];
+    for (var m = 0; m < diagonalsIndex[w].length; m++) {
+        slotsInDiagonal[w].push(slot.eq(diagonalsIndex[w][m]));
+    }
+}
 
-        console.log("slots in diagonal: ", slotsInDiagonals);
+console.log("slotsInDiagonal: ", slotsInDiagonal);
 
+$(".column").on("click", function(e) {
+    var slotsInColumn = $(e.currentTarget).find(".slot");
+    for (var i = 5; i >= 0; i--) {
         if (
-            checkForVictory(slotsInColumn) ||
-            checkForVictory(slotsInRow) ||
-            checkForVictory(slotsInDiagonals)
+            !slotsInColumn.eq(i).hasClass("player1") &&
+            !slotsInColumn.eq(i).hasClass("player2")
         ) {
-            victory(currentPlayer);
-        }
-        switchPlayers();
-    });
-
-    function switchPlayers() {
-        if (currentPlayer == "player1") {
-            currentPlayer = "player2";
-        } else if (currentPlayer == "player2") {
-            currentPlayer = "player1";
+            slotsInColumn.eq(i).addClass(currentPlayer);
+            break;
         }
     }
+    // gone thru hole loop without breake = column is full
+    // i is the row5
+    if (i == -1) {
+        return;
+    }
+    var slotsInRow = $(".column").find(".row" + i);
 
-    function checkForVictory(slots) {
-        var count = 0;
-        for (var i = 0; i < slots.length; i++) {
-            if (slots.eq(i).hasClass(currentPlayer)) {
-                count++;
-                if (count == 4) {
-                    return true;
-                }
-            } else {
-                count = 0;
+    console.log("slots in row: ", slotsInRow);
+    console.log("slots in column: ", slotsInColumn);
+    console.log("slots in diagonal: ", slotsInDiagonal);
+
+    // check diagonal slots
+    var fourInDiagonal;
+    for (var k = 0; k < slotsInDiagonal.length; k++) {
+        fourInDiagonal = checkForVictory(slotsInDiagonal[k]);
+    }
+
+    if (
+        checkForVictory(slotsInColumn) ||
+        checkForVictory(slotsInRow) ||
+        fourInDiagonal
+    ) {
+        victory(currentPlayer);
+    }
+    switchPlayers();
+});
+
+function switchPlayers() {
+    if (currentPlayer == "player1") {
+        currentPlayer = "player2";
+    } else if (currentPlayer == "player2") {
+        currentPlayer = "player1";
+    }
+}
+
+function checkForVictory(slots) {
+    var count = 0;
+    console.log("number of slots to check: ", slots.length);
+    for (var i = 0; i < slots.length; i++) {
+        if (slots.eq(i).hasClass(currentPlayer)) {
+            count++;
+            console.log("counter: ", count);
+            if (count == 4) {
+                return true;
             }
+        } else {
+            count = 0;
         }
     }
+}
 
-    function victory(player) {
-        console.log("Victory!, the winer is ", player);
-        // add message and animation
-        // restart the game - location.reload
-        // currentPlayer = player1
-    }
-    var diagonalsIndex = [
-        [0, 7, 14, 21, 28, 35],
-        [1, 8, 15, 22, 29],
-        [2, 9, 16, 23],
-        [6, 13, 20, 27, 34, 41],
-        [12, 19, 26, 33, 40],
-        [18, 25, 32, 39],
-        [18, 13, 8, 3],
-        [24, 19, 14, 9, 4],
-        [30, 25, 20, 15, 10, 5],
-        [36, 31, 26, 21, 16, 11],
-        [37, 32, 27, 22, 17],
-        [38, 33, 28, 23]
-    ];
-})();
+function victory(player) {
+    console.log("Victory!, the winer is ", player);
+    // add message and animation
+    // restart the game - location.reload
+    // currentPlayer = player1
+}
+// })();
