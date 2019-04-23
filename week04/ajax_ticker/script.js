966ca060d6b73261b95d24d315caa7fa60ee054f
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-23 15:48:08 +0200

    ajax ticker exercise

---
+++ week04/ajax_ticker/script.js
@@ -0,0 +1,42 @@
+(function() {
+    console.log("hi");
+    var headlines = document.getElementById("headlines");
+    var resultsContainer = $("#headlines");
+    var links = headlines.getElementsByTagName("A"); //number of links
+    var left = headlines.offsetLeft;
+    var animationId;
+
+    $.ajax({
+        url: "/data.json",
+        method: "GET",
+        success: function(data) {
+            console.log("Data received: ", data);
+            var resultsHtml = "";
+            for (var i = 0; i < data.length; i++) {
+                resultsHtml +=
+                    "<a href = '" + data[i].href + "'>" + data[i].text + "</a>";
+            }
+            resultsContainer.html(resultsHtml).show();
+            moveHeadlineLines();
+            console.log("Results html: ", resultsHtml);
+        }
+    });
+
+    function moveHeadlineLines() {
+        left--;
+        if (left <= -links[0].offsetWidth) {
+            left += links[0].offsetWidth;
+            headlines.appendChild(headlines.removeChild(links[0]));
+        }
+        headlines.style.left = left + "px"; //move headlines to left
+        animationId = requestAnimationFrame(moveHeadlineLines);
+    }
+
+    headlines.addEventListener("mouseenter", function() {
+        cancelAnimationFrame(animationId);
+    });
+
+    headlines.addEventListener("mouseleave", function() {
+        moveHeadlineLines();
+    });
+})();
