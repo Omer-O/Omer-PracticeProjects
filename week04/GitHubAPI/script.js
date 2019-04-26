7327bca25b11e9e2974e4fb8d94f265a1369068c
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-26 10:55:14 +0200

    add template

---
+++ week04/GitHubAPI/script.js
@@ -0,0 +1,13 @@
+(function() {
+    Handlebars.templates = Handlebars.templates || {};
+
+    var templates = document.querySelectorAll(
+        'script[type="text/x-handlebars-template"]'
+    );
+
+    Array.prototype.slice.call(templates).forEach(function(script) {
+        Handlebars.templates[script.id] = Handlebars.compile(script.innerHTML);
+    });
+
+    //-----------------------------------------------------------
+})();
