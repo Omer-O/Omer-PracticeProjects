4fbc71537748400eede6a4e41a60a620c6750b40
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-26 11:13:40 +0200

    add input

--- week04/GitHubAPI/script.js
+++ week04/GitHubAPI/script.js
@@ -10,4 +10,10 @@
     });
 
     //-----------------------------------------------------------
+
+    $(".submit-button").on("click", function() {
+        var username = $('input[name="username"]').val();
+        var password = $('input[name="password"]').val();
+        var userToSearch = $('input[name="user-to-search"]').val();
+    });
 })();
