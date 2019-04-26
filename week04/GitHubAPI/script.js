63f4254b75c10335e3e0d4562c8d8e9128f5bb5e
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-26 16:04:04 +0200

    remove logs add click on card

--- week04/GitHubAPI/script.js
+++ week04/GitHubAPI/script.js
@@ -18,23 +18,23 @@
 
         var rootUrl = "https://api.github.com";
         var endpoint = "/users/" + userToSearch + "/repos";
-        console.log("url: ", rootUrl + endpoint);
         $.ajax({
             url: rootUrl + endpoint,
             headers: {
                 Authorization: "Basic " + btoa(username + ":" + password)
             },
             success: function(payload) {
-                console.log("success payload: ", payload);
                 getResults(payload);
-                //avatar_url
-                //full_name
                 // 10 most (cut down if more) of commits when click, second ajax request with diffrenet endpoint
             }
         });
     });
+
+    $(".card").on("click", function(e) {
+        console.log("clicked on card", e);
+    });
+
     function getResults(inputPayload) {
-        console.log(inputPayload);
         $("#results-container").html(
             Handlebars.templates.cards({ results: inputPayload })
         );
