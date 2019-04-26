8d25e4b052bbcf03f507b0f63747013015d10a1e
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-26 12:39:27 +0200

    add basic result display

--- week04/GitHubAPI/script.js
+++ week04/GitHubAPI/script.js
@@ -15,5 +15,28 @@
         var username = $('input[name="username"]').val();
         var password = $('input[name="password"]').val();
         var userToSearch = $('input[name="user-to-search"]').val();
+
+        var rootUrl = "https://api.github.com";
+        var endpoint = "/users/" + userToSearch + "/repos";
+        console.log("url: ", rootUrl + endpoint);
+        $.ajax({
+            url: rootUrl + endpoint,
+            headers: {
+                Authorization: "Basic " + btoa(username + ":" + password)
+            },
+            success: function(payload) {
+                console.log("success payload: ", payload);
+                getResults(payload);
+                //avatar_url
+                //full_name
+                // 10 most (cut down if more) of commits when click, second ajax request with diffrenet endpoint
+            }
+        });
     });
+    function getResults(inputPayload) {
+        console.log(inputPayload);
+        $("#results-container").html(
+            Handlebars.templates.cards({ results: inputPayload })
+        );
+    }
 })();
