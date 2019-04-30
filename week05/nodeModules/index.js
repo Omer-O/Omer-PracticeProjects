22fe8a19e47f73aa53b6ce0aa6ffa01736f8a5ca
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-04-30 11:51:19 +0200

    fix query diplay, add parameters arr

--- week05/nodeModules/index.js
+++ week05/nodeModules/index.js
@@ -2,14 +2,27 @@ const url = require("url");
 const querystring = require("querystring");
 
 const input = process.argv[2];
+if (!input) {
+    process.exit();
+}
 const parseResults = url.parse(input);
 const queryValues = querystring.parse(parseResults.query);
 
-console.log("The protocol is ", parseResults.protocol);
-console.log("The host is ", parseResults.host);
-console.log("The hostname is ", parseResults.hostname);
-console.log("The port is ", parseResults.port);
-console.log("The pathname is ", parseResults.pathname);
-console.log("The query is ", parseResults.query);
-console.log("The value of the a parameter is", queryValues.a);
-console.log("The value of the b parameter is", queryValues.b);
+const parametersArray = [
+    "protocol",
+    "host",
+    "hostname",
+    "port",
+    "pathname",
+    "query"
+];
+
+parametersArray.forEach(function(field) {
+    console.log(`The ${field} is ${parseResults[field]}`);
+});
+
+if (queryValues) {
+    for (var n in queryValues) {
+        console.log(`The value of the ${n} parameter is ${queryValues[n]}`);
+    }
+}
