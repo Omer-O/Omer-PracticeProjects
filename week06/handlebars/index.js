8e14bdf53faa66b046809c0b7f5740786e7393be
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-05-07 17:46:04 +0200

    change fs.readdirSync to async

--- week06/handlebars/index.js
+++ week06/handlebars/index.js
@@ -11,37 +11,52 @@ app.use(express.static("projects"));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    let projectsList = fs.readdirSync(__dirname + "/projects", {
-        withFileTypes: true
-    });
-    res.render("welcome", {
-        layout: "main",
-        projects: projectsList
-    });
+    fs.readdir(
+        __dirname + "/projects",
+        { withFileTypes: true },
+        (err, projectsList) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            res.render("welcome", {
+                layout: "main",
+                projects: projectsList
+            });
+        }
+    );
 });
 
 app.get("/:name/description", (req, res) => {
-    let projectsList = fs.readdirSync(__dirname + "/projects");
-    console.log("project list: ", projectsList);
-    console.log(":name", req.params.name);
-    let projectFound = false;
-    projectsList.forEach(item => {
-        if (item == req.params.name) {
-            projectFound = true;
+    let name = req.params.name;
+    var projectFound = false;
+
+    fs.readdir(__dirname + "/projects", (err, files) => {
+        if (err) {
+            console.log(err);
+            return;
         }
-    });
-    if (projectFound) {
-        const description = require(`${__dirname}/projects/${
-            req.params.name
-        }/description.json`);
-        res.render("description", {
-            layout: "main",
-            projectName: description.name,
-            projectDescription: description.description
-        });
-    } else {
-        res.sendStatus(404);
-    }
+
+        files.forEach(item => {
+            console.log("item: ", item);
+            if (item == name) {
+                projectFound = true;
+            }
+        }); //end of forEach
+
+        if (projectFound) {
+            const description = require(`${__dirname}/projects/${
+                req.params.name
+            }/description.json`);
+            res.render("description", {
+                layout: "main",
+                projectName: description.name,
+                projectDescription: description.description
+            });
+        } else {
+            res.sendStatus(404);
+        }
+    }); //end of fs.readdir
 });
 
 app.listen(8080, () => {
