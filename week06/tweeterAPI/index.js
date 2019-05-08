73d7ddd07d4f5b51bd766a5d732125a5519dd9de
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-05-08 16:46:49 +0200

    remove console logs and tidy up

--- week06/tweeterAPI/index.js
+++ week06/tweeterAPI/index.js
@@ -8,16 +8,13 @@ app.get("/data.json", (req, res) => {
     twApi.getToken(function(err, token) {
         if (err) {
             res.sendStatus(500);
-            console.log("RACHEL ERROR!");
         } else {
             console.log("received token: ", token);
             twApi.getTweets(token, function(err, tweets) {
                 if (err) {
-                    console.log("error: ", err);
                     res.sendStatus(500);
                 } else {
                     tweets = filterTweets(tweets);
-                    console.log("filtered tweets:", tweets);
                     res.json(tweets);
                 }
             });
@@ -28,12 +25,9 @@ app.get("/data.json", (req, res) => {
 function filterTweets(input) {
     const filteredTweets = input
         .filter(item => {
-            console.log("item urls: ", item.entities.urls);
-            console.log("item urls length: ", item.entities.urls.length);
             return item.entities.urls && item.entities.urls.length == 1;
         })
         .map(item => {
-            console.log("mapping...");
             return {
                 text: item.full_text,
                 href: item.entities.urls[0].url
@@ -41,7 +35,6 @@ function filterTweets(input) {
         });
     return filteredTweets;
 }
-
 //---------------------------------------------------------------
 
 app.listen(8080, () => console.log(`I'm listening.`));
